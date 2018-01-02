68fa367920f2f5af9e02139b9088f31286a30368
Nikola1993 <31863147+Nikola1993@users.noreply.github.com>
2018-01-02 11:51:36 +0100

    Initial Commit

---
+++ Countries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Countries
+//
+//  Created by Nikola Popovic on 1/2/18.
+//  Copyright © 2018 Nikola Popovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Countries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Countries
+//
+//  Created by Nikola Popovic on 1/2/18.
+//  Copyright © 2018 Nikola Popovic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
